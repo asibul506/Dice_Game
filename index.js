4c9cc22b873bae94a30347868b97ff5cdc7fe50e
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2025-05-03 18:21:10 +0200

    Simple dice game using HTML, CSS & JS

---
+++ index.js
@@ -0,0 +1,48 @@
+//alert("Welcome to the Dice game!");
+var dice1 = Math.floor(Math.random() * 6) + 1; // 1-6
+var dice2 = Math.floor(Math.random() * 6) + 1; // 1-6
+
+if(dice1==1){
+    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
+}else if(dice1==2){
+    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
+}
+else if(dice1==3){
+    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+}
+else if(dice1==4){
+    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
+}
+else if(dice1==5){
+    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
+}else{
+    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+}
+
+
+if(dice2==1){
+document.querySelector(".img2").setAttribute("src", "images/dice1.png");
+}else if(dice2==2){
+    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
+}
+else if(dice2==3){
+    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
+}
+else if(dice2==4){
+    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
+}
+else if(dice2==5){
+    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
+}else{
+    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+}
+
+if(dice1>dice2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
+}
+else if(dice1<dice2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
